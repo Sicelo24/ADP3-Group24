7519a1525f99eeb6822c9ea7349ca33ee5d65549
Athenkosi <athenkosizono@gmail.com>
2021-03-31 16:51:14 +0200

    Added My Wolf Class and WolfTest class

--- src/test/java/za/ac/cput/project/WolfTest.java
+++ src/test/java/za/ac/cput/project/WolfTest.java
@@ -1,12 +1,25 @@
 package za.ac.cput.project;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WolfTest {
 
+    Wolf wolfR, wolfB, wolfG;
+
     @BeforeEach
     void setUp() {
+        wolfR = new Wolf("Reddie", "Red", 4.91);
+        wolfB = new Wolf("Darkie", "Black", 27.1);
+        wolfG = new Wolf("Jackie", "Green", 10.2);
     }
+
+    @Test
+    void testEquality(){
+        assertEquals(wolfR, wolfG);
+    }
+
+
 }
