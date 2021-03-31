55f66b9647b363aff307e058988e68ec0471a405
LindoXaki <215041720>
2021-04-01 00:24:18 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/project/Addition.java
@@ -0,0 +1,10 @@
+package za.ac.cput.project;
+
+public class Addition {
+
+    public void main (String[] args){
+
+        System.out.println("This is addition class");
+    }
+
+}
---
+++ src/test/java/za/ac/cput/project/AdditionTest.java
@@ -0,0 +1,23 @@
+package za.ac.cput.project;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdditionTest {
+
+
+    int firstNum;
+    int secondNum;
+    int sum;
+    int thirdNum;
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+
+
+}
