9f10c0abe27d6dc4ea717366331bc8ae138e9077
Avuyile <Xozumthiavuyile1@gmail.com>
2021-03-31 23:31:58 +0200

    comming my branch to the git

--- src/main/java/za/ac/cput/project/horse.java
+++ src/main/java/za/ac/cput/project/horse.java
@@ -1,4 +1,48 @@
+//*
+//Name:Avuyile
+//Surname:Xozumthi
+//Student Number:218331827
+//Date:31 March 2021
+//*
 package za.ac.cput.project;
+import java.util.Objects;
 
 public class horse {
+
+    private String breed;
+    private int speed;
+
+    public horse(String breed, int speed){
+        this.setBreed(breed);
+        this.setSpeed(speed);
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        horse horse = (horse) o;
+        return speed == horse.speed && Objects.equals(breed, horse.breed);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(breed, speed);
+    }
 }
