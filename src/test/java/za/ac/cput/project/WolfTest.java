435d768dd66d1d8614e1d3fd39c02a50033aa30d
Athenkosi <athenkosizono@gmail.com>
2021-03-31 16:17:58 +0200

    Added Test package

---
+++ src/test/java/za/ac/cput/project/WolfTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput.project;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WolfTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
